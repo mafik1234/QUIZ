1f838b74518046eb4285eb1f32df3e2514d0e32d
burlac <victorburlac94@gmail.com>
2018-02-25 18:41:52 +0200

    FirstComit

---
+++ data.js
@@ -0,0 +1,42 @@
+var jsonData = [
+    {
+        "q" : "Number of bit used by the IPv6 address ?",
+        "opt1" : "32",
+        "opt2" : "64",
+        "opt3" : "128",
+		"opt4" : "256",
+        "answer" : "128"
+    },
+    {
+        "q" : "Witch one programming language is exclusively used for artificial intelligence ?",
+        "opt1" : "C++",
+        "opt2" : "Java",
+        "opt3" : "PHP",
+		"opt4" : "Prolog",
+        "answer" : "Prolog"
+    },
+    {
+        "q" : "Firewall in computer is used for ?",
+        "opt1" : "Security",
+        "opt2" : "Data Transmission",
+        "opt3" : "Authentication",
+		"opt4" : "Monitoring",
+        "answer" : "Security"
+    },
+    {
+        "q" : "Number of layers in the OSI Model ?",
+        "opt1" : "9",
+        "opt2" : "3",
+        "opt3" : "7",
+		"opt4" : "11",
+        "answer" : "7"
+    },
+    {
+        "q" : "Which protocol is used to received e-mail ?",
+        "opt1" : "SMTP",
+        "opt2" :"POP3",
+        "opt3" : "HTTP",
+		"opt4" : "FTP",
+        "answer" : "POP3"
+    }
+];
